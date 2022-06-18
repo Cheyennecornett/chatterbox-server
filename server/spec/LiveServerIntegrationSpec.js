c81958e507ac72517cbdd41f04b05b82e4dc770a
Manuel Rosadilla <manurosadilla@gmail.com>
2022-06-18 12:24:19 -0400

    Finished Bare Mins

--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -60,11 +60,49 @@ describe('server', function() {
 
   it('Should 404 when asked for a nonexistent endpoint', function(done) {
     request('http://127.0.0.1:3000/arglebargle', function(error, response, body) {
-     // console.log(response)
+      // console.log(response)
       expect(response.statusCode).to.equal(404);
       done();
     });
   });
 
+  it('posted messages should contain an id prop', function(done) {
+    var requestParams = {method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/messages',
+      json: {
+        username: 'Jono',
+        text: 'Do my bidding!'}
+    };
+
+    request(requestParams, function(error, response, body) {
+      // Now if we request the log, that message we posted should be there:
+      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+        var messages = JSON.parse(body);
+        expect(typeof messages[0].message_id).to.equal('number');
+        //expect(messages[0].text).to.equal('Do my bidding!');
+        done();
+      });
+    });
+
+  });
+  it('posted messages should contain an id equal to the length of the array', function(done) {
+    var requestParams = {method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/messages',
+      json: {
+        username: 'Jono',
+        text: 'Do my bidding!'}
+    };
+
+    request(requestParams, function(error, response, body) {
+      // Now if we request the log, that message we posted should be there:
+      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+        var messages = JSON.parse(body);
+        expect(messages[0].message_id).to.equal(messages.length);
+        //expect(messages[0].text).to.equal('Do my bidding!');
+        done();
+      });
+    });
+
+  });
 
 });
